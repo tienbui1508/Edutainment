f5ffee21cce38f9eba69a0d9d244a8d0b5e408c1
tienbui <tienbui1508@gmail.com>
2023-05-29 18:14:46 +0930

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Tien Bui on 29/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
